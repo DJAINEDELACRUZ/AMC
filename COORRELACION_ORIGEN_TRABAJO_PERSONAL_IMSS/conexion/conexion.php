b9ef119ae5cc3be490cb9bebfb70d098343fc169
Djaine <djaine.sanchez@imss.gob.mx>
2025-01-10 12:41:48 -0600

    Distribución de Frecuencias por Lugar de Nacimiento y Trabajo
    Relación Origen-Trabajo: Frecuencia y Porcentaje

---
+++ COORRELACION_ORIGEN_TRABAJO_PERSONAL_IMSS/conexion/conexion.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'configuracion.php';
+
+function obtenerConexion($identificador = 'default')
+{
+    global $configuraciones;
+
+    if (!isset($configuraciones[$identificador])) {
+        die("Error: Configuración no encontrada para '$identificador'");
+    }
+
+    $config = $configuraciones[$identificador];
+
+    try {
+        $conexion = new PDO($config['dsn'], $config['usuario'], $config['contraseña'], [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+        return $conexion;
+    } catch (PDOException $e) {
+        die('Falló la conexión: ' . $e->getMessage());
+    }
+}
---
+++ COORRELACION_ORIGEN_TRABAJO_PERSONAL_IMSS/conexion/configuracion.php
@@ -0,0 +1,14 @@
+<?php
+
+$configuraciones = [
+    'default' => [
+        'dsn' => 'mysql:dbname=personalaps;host=localhost',
+        'usuario' => 'root',
+        'contraseña' => '',
+    ],
+    'servidor_secundario' => [
+        'dsn' => 'mysql:dbname=rda;host=10.102.9.88',
+        'usuario' => 'b2',
+        'contraseña' => 'DbApracM3d',
+    ],
+];
